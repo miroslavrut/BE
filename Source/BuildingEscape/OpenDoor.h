944f19d2a9a6b6a7cfa483474ef4a3a506d988b0
DESKTOP-THRIJG3\Miroslav <rutesicm27@gmail.com>
2017-10-09 17:23:37 +0200

    BE 69 Macros(UPROPERTY) and TrigerVolume

--- Source/BuildingEscape/OpenDoor.h
+++ Source/BuildingEscape/OpenDoor.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
 #include "OpenDoor.generated.h"
 
 
@@ -24,6 +25,10 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+private:
+	UPROPERTY(VisibleAnywhere)
+	float OpenAngle = 90.f;         // will be visable in editor but not editable
 	
+	UPROPERTY(EditAnywhere)
+		ATriggerVolume* PressurePlate;  // visable and editable in details of opendoor in editor
 };
